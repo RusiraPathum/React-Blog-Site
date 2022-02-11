de37a2b955c96c435947c8e230cc9bf282df2388
RusiraPathum <rusira.pathum20@gmail.com>
2022-02-11 12:58:10 +0530

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+
 import './App.css';
+import Navigation from './component/Navigation';
 
 function App() {
+
+  const title = "Welcome to the new blog";
+
   return (
     <div className="App">
-      
+      <Navigation/>
+      <div className="content">
+        <h1>{title}</h1>
+      </div>
     </div>
   );
 }
